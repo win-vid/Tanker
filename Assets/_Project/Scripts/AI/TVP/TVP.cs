767923faec7ba6a4849d47d8b11f98efc58bf982
Winston Reichelt <winstonjreichelt@gmail.com>
2025-11-16 20:22:16 +0100

    last commit

--- Assets/_Project/Scripts/AI/TVP/TVP.cs
+++ Assets/_Project/Scripts/AI/TVP/TVP.cs
@@ -1,10 +1,5 @@
 using UnityEngine;
 
-/*
-* JagdPanzer AI Manager
-* Does not need a turret
-*/
-
 public class TVP : AIStateMachine
 {
     public float explosionTimer = 2f;
@@ -25,9 +20,11 @@ public class TVP : AIStateMachine
             float angle = i * Mathf.Deg2Rad;
             Vector3 direction = new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle));
             GameObject bullet = ObjectPool.instance.GetPooledObject(projectilePrefab);
-            bullet.transform.position = transform.position + new Vector3(0, 1f, 0);     // slightly above ground
-            bullet.transform.rotation = Quaternion.LookRotation(direction);
-            bullet.SetActive(true);
+            if(bullet != null){
+                bullet.transform.position = transform.position + new Vector3(0, 1f, 0);     // slightly above ground
+                bullet.transform.rotation = Quaternion.LookRotation(direction);
+                bullet.SetActive(true);
+            }
         }
 
         RemoveFromGame();
